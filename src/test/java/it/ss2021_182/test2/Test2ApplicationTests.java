3fcc4b6116ae701bd435f491e5a8c274da358fdc
DESKTOP-QB9SM4A\leler <lelerizzo3@gmail.com>
2022-12-15 20:50:28 +0100

    progetto spring boot

---
+++ src/test/java/it/ss2021_182/test2/Test2ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.ss2021_182.test2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
